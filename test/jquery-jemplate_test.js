36aa08c13d5679b3606fc76ccbe33c6ce69bb028
John Lifsey <john.lifsey@inspire.com>
2015-05-29 19:02:30 -0400

    more tests

--- test/jquery-jemplate_test.js
+++ test/jquery-jemplate_test.js
@@ -11,11 +11,14 @@
   });
 
   test('render', function () {
-    expect(4);
+    expect(7);
     ok($('.jemplate').jemplate('test.tt', { name:'test0' }).text().match('Hello test0'), 'multiple element render');
     strictEqual($('#test1').jemplate('test.tt').text(), 'Hello data element.','single element render with html data');
     strictEqual($('#test1').jemplate('test.tt', { name:'data passed' }).text(), 'Hello data passed.','single element render with passed data');
     strictEqual($('#test1').jemplate({ jemplate:'test.tt', name:'as object' }).text(), 'Hello as object.','single element render from object config');
+    strictEqual($('#test2').jemplate({ name:'data passed' }).text(), 'Hello data passed.','template specified in html');
+    strictEqual($('#test3').jemplate().text(), 'Hello data element.','template and data specified in html');
+    strictEqual($('#test3').jemplate({ method:'append', name:'appended' }).text(), 'Hello data element.Hello appended.','append method');
   });
 
 }(jQuery));
